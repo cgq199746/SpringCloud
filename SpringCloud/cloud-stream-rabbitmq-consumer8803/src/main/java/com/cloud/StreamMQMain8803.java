db277dff7adacbd6e72dae7fb84aef06698dbe72
CGQ <1285063203@QQ.COM>
2020-11-04 22:42:39 +0800

    commit

---
+++ SpringCloud/cloud-stream-rabbitmq-consumer8803/src/main/java/com/cloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: cgq
+ * @Date: 2020/11/4 16:45
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
